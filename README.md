f986bb137ad605f8895cad3534335ad7a16282c6
Gaurav Sablok <gauravcodepro@gmail.com>
2024-10-02 20:08:31 +0200

    codecreatede
    added support for the pacbiohifi conversion from BAM files and visualization of the reads. Building the binary release version.

--- README.md
+++ README.md
@@ -3,19 +3,33 @@ go-pacbiohifi-visualization
 - bins the sequenced pacbiohifi reads for visualization
 - this should be used to see your sequencing runs distribution.
 - also takes the bam file convert them to the fastq and estimates and plots the visualization.
+- BAM support added, it converts the BAM file, reads the fastq and also puts the fasta for sending to the user.
+- Make sure that the BAM and the .pbi files are in the same folder and it will read all the BAM files for the run as a regular expression. 
 - it uses [Pterm](https://github.com/pterm/pterm) to directly give you the visualization plots.
+
 ```
-[gauravsablok@ultramarine]~/Desktop/codecreatede/golang/golang-pacbiohifi-visualization% \
-go run main.go -h
-This pacbiohifi application provides the binning intervals of the sequence classification from the sequencing reads
+gauravsablok@gaurav-sablok ~/Desktop/codecreatede/golang/golang-pacbiohifi-visualization ±main⚡ » \
+go run main.go fastq -h
+Analyzes the fastq files
 
 Usage:
-  flag [flags]
+  flag fastq [flags]
 
 Flags:
-  -b, --bamfile string     pacbiohifi bam file (default "inputfile should be bam")
-  -h, --help               help for flag
+  -h, --help               help for fastq
   -i, --inputfile string   pacbiohifi reads file (default "inputfile for the fastq reads")
+gauravsablok@gaurav-sablok ~/Desktop/codecreatede/golang/golang-pacbiohifi-visualization ±main⚡ » \
+go run main.go bam -h
+Analyzes the bam files
+
+Usage:
+  flag bam [flags]
+
+Flags:
+  -b, --bamfile string   pacbiohifi bam file or the folder containing the bam files along with the pbi files (default "inputfile should be bam")
+  -h, --help             help for bam
+
+
 ```
 
 Gaurav Sablok
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ Date 2024-9-25
 
 A golang streaming application for visualization of the pacbiohifi reads coming from the pacbiohifi sequencing.
 It takes both the fastq as well as the bam file and converts the reads to fastq and gives you the analysis.
+You should have pbtk installed in your path and it will convert the fastq to fasta and then will plot the reads.
 
 */
 
@@ -15,6 +16,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/pterm/pterm"
@@ -36,17 +38,31 @@ var (
 var rootCmd = &cobra.Command{
 	Use:  "flag",
 	Long: "This pacbiohifi application provides the binning intervals of the sequence classification from the sequencing reads",
-	Run:  flagFunc,
+}
+
+var fastqCmd = &cobra.Command{
+	Use:  "fastq",
+	Long: "Analyzes the fastq files",
+	Run:  fastqFunc,
+}
+
+var bamCmd = &cobra.Command{
+	Use:  "bam",
+	Long: "Analyzes the bam files",
+	Run:  bamFunc,
 }
 
 func init() {
-	rootCmd.Flags().
+	fastqCmd.Flags().
 		StringVarP(&inputfile, "inputfile", "i", "inputfile for the fastq reads", "pacbiohifi reads file")
-	rootCmd.Flags().
-		StringVarP(&bamfile, "bamfile", "b", "inputfile should be bam", "pacbiohifi bam file")
+	bamCmd.Flags().
+		StringVarP(&bamfile, "bamfile", "b", "inputfile should be bam", "pacbiohifi bam file or the folder containing the bam files along with the pbi files")
+
+	rootCmd.AddCommand(fastqCmd)
+	rootCmd.AddCommand(bamCmd)
 }
 
-func flagFunc(cmd *cobra.Command, args []string) {
+func fastqFunc(cmd *cobra.Command, args []string) {
 	type headerID struct {
 		id string
 	}
@@ -70,6 +86,123 @@ func flagFunc(cmd *cobra.Command, args []string) {
 	sequenceabove200K := []string{}
 
 	fOpen, err := os.Open(inputfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fRead := bufio.NewScanner(fOpen)
+	for fRead.Scan() {
+		line := fRead.Text()
+		if strings.HasPrefix(string(line), "@") {
+			header = append(header, headerID{
+				id: string(line),
+			})
+		}
+		if strings.HasPrefix(string(line), "A") || strings.HasPrefix(string(line), "T") ||
+			strings.HasPrefix(string(line), "G") ||
+			strings.HasPrefix(string(line), "C") && !strings.Contains(string(line), ";") {
+			sequence = append(sequence, sequenceID{
+				seq: string(line),
+			})
+		}
+	}
+
+	for i := range sequence {
+		if len(string(sequence[i].seq)) >= 100000 {
+			sequence100K = append(sequence100K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 100000 && len(string(sequence[i].seq)) <= 110000 {
+			sequence110K = append(sequence110K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 110000 && len(string(sequence[i].seq)) <= 120000 {
+			sequence120K = append(sequence120K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 120000 && len(string(sequence[i].seq)) <= 130000 {
+			sequence130K = append(sequence130K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 130000 && len(string(sequence[i].seq)) <= 140000 {
+			sequence140K = append(sequence140K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 140000 && len(string(sequence[i].seq)) <= 150000 {
+			sequence150K = append(sequence150K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 150000 && len(string(sequence[i].seq)) <= 160000 {
+			sequence160K = append(sequence160K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 160000 && len(string(sequence[i].seq)) <= 170000 {
+			sequence170K = append(sequence170K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 170000 && len(string(sequence[i].seq)) <= 180000 {
+			sequence180K = append(sequence180K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 180000 && len(string(sequence[i].seq)) <= 190000 {
+			sequence190K = append(sequence190K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 190000 && len(string(sequence[i].seq)) <= 200000 {
+			sequence200K = append(sequence200K, sequence[i].seq)
+		}
+		if len(string(sequence[i].seq)) > 200000 {
+			sequenceabove200K = append(sequenceabove200K, sequence[i].seq)
+		}
+
+		bars := []pterm.Bar{
+			{Label: "sequence100K", Value: len(sequence100K)},
+			{Label: "sequence110K", Value: len(sequence110K)},
+			{Label: "sequence120K", Value: len(sequence120K)},
+			{Label: "sequence130K", Value: len(sequence130K)},
+			{Label: "sequence140K", Value: len(sequence140K)},
+			{Label: "sequence150K", Value: len(sequence150K)},
+			{Label: "sequence160K", Value: len(sequence160K)},
+			{Label: "sequence170K", Value: len(sequence170K)},
+			{Label: "sequence180K", Value: len(sequence180K)},
+			{Label: "sequence190K", Value: len(sequence190K)},
+			{Label: "sequence200K", Value: len(sequence200K)},
+			{Label: "sequenceabove200K", Value: len(sequenceabove200K)},
+		}
+		pterm.Info.Println("Profiling your Pacbiohifi sequencing reads")
+		pterm.DefaultBarChart.WithBars(bars).Render()
+
+	}
+}
+
+func bamFunc(cmd *cobra.Command, args []string) {
+	fastq, err := exec.Command("bam2fastq", "-o", "pacbio", "*.bam").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(fastq))
+	fasta, err := exec.Command("bam2fasta", "-o", "pacbio", "*.bam").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fasta)
+	out, err := exec.Command("gunzip", "pacbio.fastq.gz").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(out)
+	type headerID struct {
+		id string
+	}
+	type sequenceID struct {
+		seq string
+	}
+
+	header := []headerID{}
+	sequence := []sequenceID{}
+	sequence100K := []string{}
+	sequence110K := []string{}
+	sequence120K := []string{}
+	sequence130K := []string{}
+	sequence140K := []string{}
+	sequence150K := []string{}
+	sequence160K := []string{}
+	sequence170K := []string{}
+	sequence180K := []string{}
+	sequence190K := []string{}
+	sequence200K := []string{}
+	sequenceabove200K := []string{}
+
+	fOpen, err := os.Open("pacbio.fastq")
 	if err != nil {
 		log.Fatal(err)
 	}
